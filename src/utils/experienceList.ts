6ddf7c740a1c208cb3b9a1f5a136723922c41e87
Oghenetega Esedere <arcteggzz@gmail.com>
2024-08-22 21:14:52 +0100

    feat: updated experience

--- src/utils/experienceList.ts
+++ src/utils/experienceList.ts
@@ -1,10 +1,17 @@
 const experienceList = [
   {
-    date: "2023 - .",
+    date: "2024 - .",
+    companyName: "Sterling Bank Holdings",
+    position: "Software Engineer",
+    description:
+      "Currently, I work as a software Engineer on teh Entrepreneur in Residence team, building innovative solutions. ",
+  },
+  {
+    date: "2023 - 2024.",
     companyName: "IT Horizons Zojatech",
     position: "Frontend Developer",
     description:
-      "I am part of a team of developers responsible for developing React based frontend applications. We have built products to meet clients expectations.",
+      "I was part of a team of developers responsible for developing React based frontend applications. We have built products to meet clients expectations.",
   },
   {
     date: "2022 - 2023",
